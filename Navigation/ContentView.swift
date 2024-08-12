14fdcb0638b6963a9e5cfea07b88d023ed36e012
R. Kukuh <rkukuh@gmail.com>
2024-08-12 10:21:15 +0700

    Finish watch multiple different types of data

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,23 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    let players = [
-        "Roy Kent",
-        "Richard Montlaur",
-        "Dani Rojas",
-        "Jamie Tartt",
-    ]
-    
     var body: some View {
         NavigationStack {
-            List(players, id: \.self) { player in
-                NavigationLink {
-                    PlayerView(name: player)
-                } label: {
-                    Text(player)
-                }
+            List {
+                NavigationLink("Show an integer", value: 42)
+                NavigationLink("Show a string", value: "Hello, world!")
+                NavigationLink("Show a Double", value: Double.pi)
             }
-            .navigationTitle("Select a player")
+            .navigationTitle("Select a value")
+            .navigationDestination(for: Int.self) { Text("Received Int: \($0)") }
+            .navigationDestination(for: String.self) { Text("Received String: \($0)") }
+            .navigationDestination(for: Double.self) { Text("Received Double: \($0)") }
         }
     }
 }

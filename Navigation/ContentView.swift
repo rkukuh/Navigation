7a08b52d598687fdb07075e1433b3f5625eb4d25
R. Kukuh <rkukuh@gmail.com>
2024-08-12 09:50:20 +0700

    Finish make small navigation titles

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         NavigationStack {
             Text("SwiftUI")
                 .navigationTitle("Welcome")
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }

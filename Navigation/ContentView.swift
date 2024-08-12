289a8778de1896688eef676aba3bbabefabad316
R. Kukuh <rkukuh@gmail.com>
2024-08-12 10:08:20 +0700

    Finish pushing the new view on the stack for us along with animation

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            Text("SwiftUI")
-                .navigationTitle("Welcome")
-                .navigationBarTitleDisplayMode(.inline)
+            VStack {
+                NavigationLink("Show Detail View") {
+                    DetailView()
+                }
+            }
+            .navigationTitle("Navigation")
         }
     }
 }
---
+++ Navigation/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Navigation
+//
+//  Created by R. Kukuh on 12/08/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("This is the detail view")
+    }
+}
+
+#Preview {
+    DetailView()
+}

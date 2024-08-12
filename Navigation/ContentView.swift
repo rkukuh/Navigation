f49ffd311955d2550c229cf1f8de5546df2199b1
R. Kukuh <rkukuh@gmail.com>
2024-08-12 10:33:25 +0700

    Finish using NavigationPath() if need a heterogeneous data (not just a simple array)

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var presentedNumbers = [Int]()
+    @State private var navPath = NavigationPath()
     
     var body: some View {
-        NavigationStack(path: $presentedNumbers) {
+        NavigationStack(path: $navPath) {
+            Button("Jump to random") {
+                navPath.append(Int.random(in: 1..<50))
+            }
+            
             List(1..<50) { i in
-                NavigationLink(value: i) {
+                NavigationLink(value: "Row \(i)") {
                     Label("Row \(i)", systemImage: "\(i).circle")
                 }
             }
             .navigationDestination(for: Int.self) { i in
-                VStack {
-                    Text("Detail \(i)")
-                    
-                    Button("Go to Next") {
-                        presentedNumbers.append(i + 1)
-                    }
-                }
+                Text("Int Detail \(i)")
+            }
+            .navigationDestination(for: String.self) { i in
+                Text("String Detail \(i)")
             }
             .navigationTitle("Navigation")
         }

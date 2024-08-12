39dec29cdc463ec7bfd89640d4bef6f03cfdc4a3
R. Kukuh <rkukuh@gmail.com>
2024-08-12 09:41:46 +0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by R. Kukuh on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

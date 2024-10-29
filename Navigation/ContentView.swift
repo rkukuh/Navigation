f42c6728b42376a30f4ffb94bb5fa08d9449e0a4
R. Kukuh <rkukuh@gmail.com>
2024-10-29 11:03:31 +0700

    Finish chapter

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            Text("SwiftUI")
-                .navigationTitle("Welcome")
-                .navigationBarTitleDisplayMode(.inline)
+        NavigationSplitView {
+            Text("Sidebar")
+                .navigationSplitViewColumnWidth(100)
+        } content: {
+           Text("Content")
+                .navigationSplitViewColumnWidth(200)
+        } detail: {
+            Text("Detail")
         }
     }
 }
